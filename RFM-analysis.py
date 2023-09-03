45c01d05b930d46e2f98555b271b142ceaaf7061
msajeenah <142747145+msajeenah@users.noreply.github.com>
2023-09-04 00:08:14 +0300

    Update RFM-analysis.py

--- RFM-analysis.py
+++ RFM-analysis.py
@@ -33,13 +33,11 @@ def main(argv):
    try:
       opts, args = getopt.getopt(argv,"hi:o:d:")
    except getopt.GetoptError:
-      print "RFM-analysis.py -i <orders.csv> -o <rfm-table.csv> -d <yyyy-mm-dd>"
+   
       sys.exit(2)
    for opt, arg in opts:
-      if opt == '-h':
-         print "RFM-analysis.py -i <orders.csv> -o <rfm-table.csv> -d <yyyy-mm-dd>"
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
+   
+      if opt in ("-i", "--ifile"):
          inputfile = arg
       elif opt in ("-o", "--ofile"):
          outputfile = arg   
